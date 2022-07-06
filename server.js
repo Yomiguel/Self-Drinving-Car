4d69aca5938e0083b476a641066dd5365117c830
Luis <yomiguel112@gmail.com>
2022-07-06 18:05:20 -0500

    server started

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
